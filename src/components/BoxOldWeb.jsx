63889e4ca3148826aa6bfb426f5c47457628146f
Chenkwand22 <chenkwand8@gmail.com>
2024-09-10 20:48:38 +0700

    Add files via upload

---
+++ src/components/BoxOldWeb.jsx
@@ -0,0 +1,16 @@
+
+const BoxOldWeb = () => {
+	return (
+		<div id="BoxOldWeb">
+			<a href="https://msha.ke/tejekate3?fbclid=PAZXh0bgNhZW0CMTEAAabcYw5xIetXYhnzWW3e_GYstsea9L6Qkf2P2ZArFcZzU1J9xoRzqgDlL_A_aem_ATlK-ZF0jO01x_e75NeQeNb6YP1BPKabrLoRDXbVNmYh_DeswVse344H12KzPYxow-TcziZjO2AVk7TOyXPKPS1m">
+				<div className="flex justify-between">
+					<img src="/link.png" alt="" className="w-auto h-6" />
+					<img src="/next.png" alt="" className="h-3 w-3" />
+				</div>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Old Website</h1>
+			</a>
+		</div>
+	)
+}
+
+export default BoxOldWeb
